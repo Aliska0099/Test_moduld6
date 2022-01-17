77e547512e39bfe290a55046049b5e5fbb552be6
Aliska0099 <91954665+Aliska0099@users.noreply.github.com>
2022-01-17 16:23:20 +0200

    Delete forms.py

--- newsportal/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, BooleanField
-from .models import Post
-
-
-class NewsForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['author', 'categoryType', 'category', 'title', 'text']
